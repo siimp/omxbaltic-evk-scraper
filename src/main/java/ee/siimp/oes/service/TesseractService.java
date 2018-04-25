1b4b2634ab680472fac38be5ed10c04809de97a3
siim puustusmaa <siimpuustusmaa@gmail.com>
2018-04-26 02:14:30 +0300

    refactor + libraries update

---
+++ src/main/java/ee/siimp/oes/service/TesseractService.java
@@ -0,0 +1,54 @@
+package ee.siimp.oes.service;
+
+import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.javacpp.tesseract;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+import static org.bytedeco.javacpp.opencv_core.Mat;
+
+@Service
+public class TesseractService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TesseractService.class);
+
+    private static final String CHAR_WHITELIST = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+
+    @Value("classpath:tesseract")
+    private Resource tesseractConfigPath;
+
+    public String readSingleLetter(Mat image) {
+        String result = null;
+        try (tesseract.TessBaseAPI api = new tesseract.TessBaseAPI();) {
+            if (api.Init(tesseractConfigPath.getFile().getAbsolutePath(), "eng") != 0) {
+                LOG.error("Tesseract config not found in {}", tesseractConfigPath.getFile().getAbsolutePath());
+                return null;
+            }
+            api.SetVariable("tessedit_char_whitelist", CHAR_WHITELIST);
+            //api.SetVariable("language_model_penalty_non_dict_word", "0");
+            //api.SetVariable("language_model_penalty_non_freq_dict_word", "0");
+
+            api.SetImage(image.arrayData(), image.size().width(), image.size().height(), image.channels(), (int) image.step1());
+
+            result = getText(api);
+            api.End();
+            return result;
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
+        return result;
+    }
+
+    private String getText(tesseract.TessBaseAPI api) {
+        try (BytePointer outText = api.GetUTF8Text()) {
+            return outText.getString().replaceAll(" ", "").trim();
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
+
+        return null;
+    }
+}
--- src/main/java/ee/siimp/oes/utils/OpenCvUtil.java
+++ src/main/java/ee/siimp/oes/utils/OpenCvUtil.java
@@ -1,106 +1,97 @@
 package ee.siimp.oes.utils;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.bytedeco.javacpp.opencv_core;
-import org.bytedeco.javacpp.opencv_core.Mat;
-import org.bytedeco.javacpp.opencv_core.MatVector;
-import org.bytedeco.javacpp.opencv_core.Rect;
-import org.bytedeco.javacpp.opencv_core.Scalar;
-import org.bytedeco.javacpp.opencv_core.Size;
 import org.bytedeco.javacpp.opencv_imgcodecs;
 import org.bytedeco.javacpp.opencv_imgproc;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.bytedeco.javacpp.opencv_core.Mat;
+import static org.bytedeco.javacpp.opencv_core.Rect;
+
 
 public class OpenCvUtil {
-    
-    public static final Scalar WHITE = new Scalar(org.bytedeco.javacpp.helper.opencv_core.CV_RGB(255, 255, 255));
-    public static final Scalar RED = new Scalar(org.bytedeco.javacpp.helper.opencv_core.CV_RGB(255, 0, 0));
-    public static final Scalar GREEN = new Scalar(org.bytedeco.javacpp.helper.opencv_core.CV_RGB(0, 255, 0));
 
-    private static final int MIN_LETTER_WIDTH = 4;
-    private static final int MAX_LETTER_WIDTH = 40;
-    private static final int MIN_LETTER_HEIGHT = 10;
-    private static final int MAX_LETTER_HEIGHT = 50;
-    private static final int MIN_POINT_SIZE = 3;
-    private static final int MAX_POINT_SIZE = 6;
-    
-    public static Mat readImageFromFile(String fileName) {
+    private static final Logger LOG = LoggerFactory.getLogger(OpenCvUtil.class);
+    private static final int LETTER_WIDTH = 10;
+
+    private OpenCvUtil() {
+    }
+
+    public static Mat readImage(String fileName) {
         return opencv_imgcodecs.imread(fileName);
-    }
-
-    public static Mat toGrayImage(Mat image) {
-        Mat grayImage = new Mat();
-        opencv_imgproc.cvtColor(image, grayImage, opencv_imgproc.CV_BGR2GRAY);
-        return grayImage;
-    }
-
-    public static Mat toThresholdImage(Mat image, int threshold, int algorithm) {
-        Mat thresholdImage = new Mat();
-        opencv_imgproc.threshold(image, thresholdImage, threshold, 255, algorithm);
-        return thresholdImage;
-    }
-
-
-
-    public static Mat createContourMask(Mat srcImage, MatVector contours, Mat debugImage) {
-        Mat maskImage = Mat.zeros(srcImage.size(), opencv_core.CV_8UC1).asMat();
-
-        for (int i = 0; i < contours.size(); i++) {
-            try(Rect r = opencv_imgproc.boundingRect(contours.get(i))) {
-                if (isLetterContour(r) || isDot(r)) { 
-                    opencv_imgproc.drawContours(maskImage, contours, i, WHITE, 
-                            opencv_imgproc.CV_FILLED, opencv_core.LINE_8, null, Integer.MAX_VALUE, null);
-                    if (debugImage != null) {
-                        opencv_imgproc.drawContours(debugImage, contours, i, RED);
-                    }
-                } else if (debugImage != null) {
-                    opencv_imgproc.drawContours(debugImage, contours, i, GREEN);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            
-        }
-        return maskImage;
-    }
-
-    private static boolean isDot(Rect r) {
-        return r.height() < MAX_POINT_SIZE && r.height() > MIN_POINT_SIZE && r.width() < MAX_POINT_SIZE && r.width() > MIN_POINT_SIZE;
-    }
-
-    private static boolean isLetterContour(Rect r) {
-        return r.height() < MAX_LETTER_HEIGHT && r.height() > MIN_LETTER_HEIGHT && r.width() < MAX_LETTER_WIDTH && r.width() > MIN_LETTER_WIDTH;
-    }
-
-    public static Mat createContourMask(Mat image, MatVector contours) {
-        return createContourMask(image, contours, null);
-    }
-    
-
-    public static void applyMask(Mat image, Mat cleanedImage, Mat maskImage) {
-        opencv_core.bitwise_and(image, image, cleanedImage, maskImage);
     }
 
     public static void writeImage(String fileName, Mat image) {
         opencv_imgcodecs.imwrite(fileName, image);
     }
 
-    @SuppressWarnings("resource")
+    public static Mat toGrayColorImage(Mat image) {
+        Mat result = new Mat();
+        opencv_imgproc.cvtColor(image, result, opencv_imgproc.CV_BGR2GRAY);
+        return result;
+    }
+
     public static Mat toBlurredImage(Mat image, int size) {
-        Mat blurredImage = new Mat();
-        opencv_imgproc.blur(image, blurredImage, new Size(size, size));
-        return blurredImage;
-    }
-    
-    @SuppressWarnings("resource")
-    public static Mat toGaussianBlurredImage(Mat image, int size, double sigmaX) {
-        Mat blurredImage = new Mat();
-        opencv_imgproc.GaussianBlur(image, blurredImage, new Size(size, size), sigmaX);
-        return blurredImage;
+        Mat result = new Mat();
+        opencv_imgproc.blur(image, result, new opencv_core.Size(size, size));
+        return result;
     }
 
-    public static Mat toMedianBlurredImage(Mat image, int size) {
-        Mat blurredImage = new Mat();
-        opencv_imgproc.medianBlur(image, blurredImage, size);
-        return blurredImage;
+    public static Mat toThresholdImage(Mat image, int threshold) {
+        Mat result = new Mat();
+        opencv_imgproc.threshold(image, result, threshold, 255, opencv_imgproc.CV_THRESH_BINARY);
+        return result;
     }
 
+    public static List<Mat> toLetterImages(Mat image, int lettersOnImage) {
+        List<MatWithLocation> letters = new ArrayList<>();
+        try (opencv_core.MatVector contours = new opencv_core.MatVector(); Mat hierarhy = new Mat();) {
+            opencv_imgproc.findContours(image, contours, hierarhy, opencv_imgproc.CV_RETR_TREE, opencv_imgproc.CV_CHAIN_APPROX_SIMPLE);
+
+            for (int i = 0; i < contours.size(); i++) {
+                try (opencv_core.Rect r = opencv_imgproc.boundingRect(contours.get(i))) {
+                    if (isFullImageContour(r, image) || !isPotensialLetter(r)) {
+                        continue;
+                    }
+                    Mat letterImage = image.apply(r).adjustROI(5, 0, 0, 0);
+                    letters.add(new MatWithLocation(letterImage, r.x()));
+                } catch (Exception e) {
+                    LOG.error(e.getMessage(), e);
+                }
+            }
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
+
+        return letters.stream()
+                .sorted(Comparator.comparingInt(MatWithLocation::getX))
+                .limit(lettersOnImage)
+                .map(MatWithLocation::getMat)
+                .collect(Collectors.toList());
+    }
+
+    private static boolean isPotensialLetter(Rect r) {
+        return r.width() >= LETTER_WIDTH;
+    }
+
+    private static boolean isFullImageContour(opencv_core.Rect rect, Mat image) {
+        return rect.size().width() == image.size().width();
+    }
+
+
+}
+
+@Getter
+@AllArgsConstructor
+class MatWithLocation {
+    private Mat mat;
+    private int x;
 }
